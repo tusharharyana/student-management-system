02ccc3f4d2708a1f938ff735d9c3353f65978f0c
tusharharyana <tusharkashyap203@gmail.com>
2026-06-04 14:56:04 +0530

    Day 2 oops concept done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -3,15 +3,14 @@ package com.example.sms.controller;
 import com.example.sms.model.Student;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/students")
 @CrossOrigin(origins = "http://localhost:5173")
-public class StudentController{
+public class StudentController {
 
+    @GetMapping
     public Student getStudent() {
         return new Student(1, "John Doe", "Computer Science");
     }
-
 
 }
