d0871c84976c6109fb858b7e93ff12191d12d91e
tusharharyana <tusharkashyap203@gmail.com>
2026-06-04 16:08:44 +0530

    Day 2 react integration completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function App(){
+    
+    // {"id":1,"name":"John Doe","course":"Computer Science"}
+
+    const [student, setStudent] = useState(null);
+
+    const getStudent = async () => {
+
+            const response = await fetch('http://localhost:8080/students');
+            const data = await response.json();
+            setStudent(data);
+
+    }
+
     return (
         <div>
-            <h1>hello</h1>
+
+            <button onClick={getStudent}>
+                Fetch Student
+            </button>
+
+            <br />
+            <h1>Here will be student data:</h1>
+
+            <h1>{student?.name}</h1>
+            <h2>{student?.course}</h2>
+            <h3>{student?.id}</h3>
+
         </div>
     )
 }
+
+export default App;
