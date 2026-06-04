7a3d42ea913850edbeb4ff3a2f0fedda93ce123f
tusharharyana <tusharkashyap203@gmail.com>
2026-06-04 12:27:10 +0530

    Day 2 setup ready for oops concepts

---
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.sms.controller;
+
+import com.example.sms.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/students")
+@CrossOrigin(origins = "http://localhost:5173")
+public class StudentController{
+
+    public Student getStudent() {
+        return new Student(1, "John Doe", "Computer Science");
+    }
+
+
+}
